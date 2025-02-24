c87b8618014561c4a13ba447a8e49dbfb78c09d4
daniel hodgson <45772010+DanielCHodgson@users.noreply.github.com>
2025-02-24 21:19:58 +0000

    Renamed template.hmtl title

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import "./styles/reset-modern.css";
 import "./styles/styles.css";
-
 import nav from "./components/Nav/Nav.js"
 import dashboard from "./components/Dashboard/Dashboard.js";
-
-
 
 const navModule = nav();
 navModule.render();
